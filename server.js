c192b663c23954fa4e51e3809ec3b0ad9ddb6e02
Donald Mohlala <137010579+ByteBeem@users.noreply.github.com>
2024-01-13 14:58:20 +0200

    Update server.js

--- server.js
+++ server.js
@@ -139,6 +139,8 @@ io.on('connection', (socket) => {
   }
 });
 
-server.listen(3000, () => {
-  console.log('Server listening on port 3000');
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
 });
+
